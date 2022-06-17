461ab249176478dae2f421ad8a0e59bdc031de43
Gabriel Natali <gabriel.natali@inovaipda.org.br>
2022-06-17 19:11:42 -0300

    Lançamento

---
+++ GestaoMusical_Web/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using GestaoMusical_Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Data;
+using System.Security.Claims;
+
+namespace GestaoMusical_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult Login(string usuario, string senha)
+        {
+            //42808193866
+            //11111111111
+            var CPF = senha.Substring(0, 3) + "," + senha.Substring(3, 3) +","+ senha.Substring(6,3) + "-" + senha.Substring(9,2);
+
+            Banco banco = new Banco();
+            var scriptSQL = "SELECT * FROM sc_alunas.tb_alunas WHERE nome_completo = '"+ usuario+"' AND cpf_aluna = '"+CPF+"' ";
+
+
+            DataTable RespostaSQL = banco.SelecionaDados(scriptSQL);
+
+            var ID = RespostaSQL.Rows[0].Field<Int64>("id_alunas").ToString();
+            if (RespostaSQL.Rows.Count > 0)
+            {
+
+                ViewData["Nome"] = usuario;
+
+                scriptSQL = "SELECT * FROM sc_alunas.tb_anotacao WHERE id_alunas ="+ ID;
+                RespostaSQL = banco.SelecionaDados(scriptSQL);
+                if (RespostaSQL.Rows.Count > 0)
+                {
+                    var anotacoes = RespostaSQL.Rows[0].Field<String>("anotacao");
+                    ViewData["Anotacao"] = anotacoes;
+                }
+            return View("Anotacao");
+
+            }
+            else { ModelState.AddModelError("", "Login Inválido."); }
+            return View("Index");
+        }
+    }
+}
---
+++ GestaoMusical_Web/Models/Banco.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Npgsql;
+
+    namespace GestaoMusical_Web.Models
+    {
+    public class Banco
+    { 
+        private NpgsqlConnection ConexaoBanco = new NpgsqlConnection();
+        /// <summary>
+        /// Procedure interna de abertura de conexao com banco de dados
+        /// </summary>
+        private void AbreBanco()
+        {
+            try
+            {
+                //ConexaoBanco = new NpgsqlConnection("Server=localhost"
+                //+ ";Port=5432;Database=db_alunas"
+                //+ ";User Id=postgres;Password=postgres;");
+                //ConexaoBanco.Open();
+
+                ConexaoBanco = new NpgsqlConnection("Server=52.73.155.171"
+                + ";Port=5432;Database=du09dbs93d50e"
+                + ";User Id=qrayepwxuyemqv; Password=5e4e372c098fda0e8fdac14e57eaee218f53c8e8aa117b25634d65d12320d41f;");
+                ConexaoBanco.Open();
+            }
+            catch
+            { }
+        }
+
+        /// <summary>
+        /// Procedure interna de fechamendo de conexão com banco
+        /// </summary>
+        private void FechaBanco()
+        {
+            try
+            {
+                ConexaoBanco.Close();
+                ConexaoBanco.Dispose();
+            }
+            catch { }
+        }
+
+        /// <summary>
+        /// Função de Seleção de Dados e preenchimento em um DataSet
+        /// </summary>
+
+        public DataSet SelecionaDadosDataSet(string SQL, string NomeDataSet, string NomeBanco = null)
+        {
+            DataSet Selecao = new DataSet(NomeDataSet);
+            AbreBanco();
+            NpgsqlCommand Comando = new NpgsqlCommand(SQL.ToUpper(), ConexaoBanco);
+            NpgsqlDataAdapter Adaptador = new NpgsqlDataAdapter(Comando);
+            Adaptador.Fill(Selecao, NomeDataSet);
+            FechaBanco();
+            return Selecao;
+        }
+
+        /// <summary>
+        /// Função de Seleção de Dados em um DataTable
+        /// </summary>
+
+        public DataTable SelecionaDados(string SQL, string NomeBanco = null)
+        {
+            DataTable Selecao = new DataTable("SELECAO");
+            AbreBanco();
+            NpgsqlCommand Comando = new NpgsqlCommand(SQL, ConexaoBanco);
+            NpgsqlDataAdapter Adaptador = new NpgsqlDataAdapter(Comando);
+            Adaptador.Fill(Selecao);
+            FechaBanco();
+            return Selecao;
+        }
+
+        /// <summary>
+        /// Função de Seleção de Dados com Passagem de parâmetros no SQL (evitar injeção SQL malicioso)
+        /// onde Parâmetros devem ser passados com o separador | (Pipe)
+        /// </summary>
+
+        public DataTable SelecionaDados(string SQL, string PARAMETROS, string NomeBanco = null)
+        {
+            DataTable Selecao = new DataTable("SELECAO");
+            try
+            {
+                AbreBanco();
+                NpgsqlCommand Comando = new NpgsqlCommand(SQL.ToUpper(), ConexaoBanco);
+                string[] aParametros = PARAMETROS.Split('|');
+                foreach (string sParametros in aParametros)
+                {
+                    if (sParametros != "")
+                    {
+                        string[] aValor = sParametros.Split(',');
+                        Comando.Parameters.Add(new NpgsqlParameter(aValor[0], aValor[1].ToUpper()));
+                    }
+                }
+                NpgsqlDataAdapter Adaptador = new NpgsqlDataAdapter(Comando);
+                Adaptador.Fill(Selecao);
+            }
+            catch { }
+            FechaBanco();
+            return Selecao;
+        }
+
+        /// <summary>
+        /// Função geral de Inserção de Dados
+        /// </summary>
+
+        public int InsereDados(string SQL, string NomeBanco = null, bool Scalar = false)
+        {
+            int iRetorno = 0;
+            try
+            {
+                AbreBanco();
+                NpgsqlCommand Comando = new NpgsqlCommand(SQL, ConexaoBanco);
+                iRetorno = Scalar == false ? Comando.ExecuteNonQuery() : Convert.ToInt32(Comando.ExecuteScalar());
+            }
+            catch (Exception ex) { }
+            FechaBanco();
+            return iRetorno;
+        }
+
+
+
+        /// <summary>
+        /// Função geral de Atualização de Dados
+        /// </summary>
+        /// <param name="SQL"></param>
+        /// <returns></returns>
+        public int AtualizaDados(string SQL, string NomeBanco = null)
+        {
+            int iRetorno = 0;
+            try
+            {
+                AbreBanco();
+                NpgsqlCommand Comando = new NpgsqlCommand(SQL, ConexaoBanco);
+                iRetorno = Comando.ExecuteNonQuery();
+            }
+            catch { }
+            FechaBanco();
+            return iRetorno;
+        }
+
+        /// <summary>
+        ///  Função geral de Atualização de Dados
+        /// </summary>
+        /// <param name="SQL"></param>
+        /// <param name="PARAMETROS"></param>
+        /// <returns></returns>
+        public int AtualizaDados(string SQL, string PARAMETROS, string NomeBanco = null)
+        {
+            int iRetorno = 0;
+            try
+            {
+                AbreBanco();
+                NpgsqlCommand Comando = new NpgsqlCommand(SQL.ToUpper(), ConexaoBanco);
+                string[] aParametros = PARAMETROS.Split('|');
+                foreach (string sParametros in aParametros)
+                {
+                    if (sParametros != "")
+                    {
+                        string[] aValor = sParametros.Split(',');
+                        if (aValor[1].ToUpper() != "")
+                            Comando.Parameters.Add(new NpgsqlParameter(aValor[0], aValor[1].ToUpper()));
+                        else
+                            Comando.Parameters.Add(new NpgsqlParameter(aValor[0], DBNull.Value));
+                    }
+                }
+                iRetorno = Comando.ExecuteNonQuery();
+            }
+            catch { }
+            FechaBanco();
+            return iRetorno;
+        }
+
+        /// <summary>
+        /// Função geral para Apagar Dados
+        /// </summary>
+        /// <param name="SQL"></param>
+        /// <returns></returns>
+        public int ApagarDados(string SQL, string NomeBanco = null)
+        {
+            int iRetorno = 0;
+            try
+            {
+                AbreBanco();
+                NpgsqlCommand Comando = new NpgsqlCommand(SQL, ConexaoBanco);
+                iRetorno = Comando.ExecuteNonQuery();
+            }
+            catch { }
+            FechaBanco();
+            return iRetorno;
+        }
+    }
+}
+
+
