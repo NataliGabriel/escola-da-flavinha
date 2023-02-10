bfbca4077d21a50f14dc3ee03e18e09f0cf57bf5
NataliGabriel <bielnatali7@gmail.com>
2023-02-10 16:50:59 -0300

    Adição da Área Professora

--- GestaoMusical_Web/Controllers/HomeController.cs
+++ GestaoMusical_Web/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace GestaoMusical_Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         public static int _id = 0;
-        public static string[] _name = null;
+        public static string _name = null;
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -37,7 +37,38 @@ namespace GestaoMusical_Web.Controllers
         [HttpPost]
         public IActionResult Login(string usuario, string senha)
         {
-            if (usuario != "André Rodrigues" && senha != "encarregadovilalaurea")
+            if (usuario == "André Rodrigues" && senha == "encarregadovilalaurea")
+            {
+                Banco banco = new Banco();
+                var scriptSQL = "SELECT * FROM sc_alunas.tb_alunas WHERE nome_completo = 'Ana Beatriz Ortune Rodrigues'";
+
+
+                DataTable RespostaSQL = banco.SelecionaDados(scriptSQL);
+
+                var ID = RespostaSQL.Rows[0].Field<Int64>("id_alunas").ToString();
+                ListaNomeAluna(Convert.ToInt32(ID));
+                ListaDataAnotacao(Convert.ToInt32(ID));
+
+                ViewData["Nome"] = "Irmão André";
+                return View("Encarregado");
+            }
+            else if ( usuario == "Flavia" && senha == "melhorinstrutora")
+            {
+
+                Banco banco = new Banco();
+                var scriptSQL = "SELECT * FROM sc_alunas.tb_alunas WHERE nome_completo = 'Ana Beatriz Ortune Rodrigues'";
+
+
+                DataTable RespostaSQL = banco.SelecionaDados(scriptSQL);
+
+                var ID = RespostaSQL.Rows[0].Field<Int64>("id_alunas").ToString();
+                ListaNomeAluna(Convert.ToInt32(ID));
+
+                ViewData["Nome"] = "Flávia";
+                return View("Encarregado");
+
+            }
+            else
             {
                 var CPF = senha.Substring(0, 3) + "," + senha.Substring(3, 3) + "," + senha.Substring(6, 3) + "-" + senha.Substring(9, 2);
 
@@ -51,8 +82,9 @@ namespace GestaoMusical_Web.Controllers
                 if (RespostaSQL.Rows.Count > 0)
                 {
                     ListaDataAnotacao(Convert.ToInt32(_id));
-                    _name = usuario.Split(" ");
-                    ViewData["Nome"] = _name[0];
+                    var user = usuario.Split(" ");
+                    _name = user[0];
+                    ViewData["Nome"] = _name;
 
                     scriptSQL = "SELECT * FROM sc_alunas.tb_anotacao WHERE id_alunas =" + _id + " ORDER BY id_anotacao DESC limit 1";
                     RespostaSQL = banco.SelecionaDados(scriptSQL);
@@ -65,21 +97,6 @@ namespace GestaoMusical_Web.Controllers
 
                 }
                 else { ModelState.AddModelError("", "Login Inválido."); return View("Index"); }
-            }
-            else
-            {
-
-
-                Banco banco = new Banco();
-                var scriptSQL = "SELECT * FROM sc_alunas.tb_alunas WHERE nome_completo = 'Ana Beatriz Ortune Rodrigues'";
-
-
-                DataTable RespostaSQL = banco.SelecionaDados(scriptSQL);
-
-                var ID = RespostaSQL.Rows[0].Field<Int64>("id_alunas").ToString();
-                ListaNomeAluna(Convert.ToInt32(ID));
-                ViewData["Nome"] = "Irmão André";
-                return View("Encarregado");
             }
         }
 
@@ -107,8 +124,8 @@ namespace GestaoMusical_Web.Controllers
         {
             Banco banco = new Banco();
             ListaDataAnotacao(_id);
-            ViewData["Nome"] = _name[0];
-            var scriptSQL = "SELECT * FROM sc_alunas.tb_anotacao WHERE id_anotacao =" + dataid+" ORDER BY id_anotacao DESC limit 1";
+            ViewData["Nome"] = _name;
+            var scriptSQL = "SELECT * FROM sc_alunas.tb_anotacao WHERE id_anotacao =" + dataid + " ORDER BY id_anotacao DESC limit 1";
             DataTable RespostaSQL = banco.SelecionaDados(scriptSQL);
             if (RespostaSQL.Rows.Count > 0)
             {
@@ -148,20 +165,20 @@ namespace GestaoMusical_Web.Controllers
             List<Login> ListagemAlunas = dt.ListaDatas(alunaID);
             string sql = "SELECT DISTINCT on (data_anotacao) data_anotacao FROM sc_alunas.tb_anotacao WHERE id_alunas =" + alunaID + "ORDER BY data_anotacao ASC limit 1";
             DataTable data = banco.SelecionaDados(sql);
-                if (ListagemAlunas.Count != 0)
+            if (ListagemAlunas.Count != 0)
+            {
+                for (int i = 0; i < ListagemAlunas.Count; i++)
                 {
-                    for (int i = 0; i < ListagemAlunas.Count; i++)
+                    ListagemAluna.Add(new SelectListItem// dd/mm/yyyy 00:00:00
                     {
-                        ListagemAluna.Add(new SelectListItem// dd/mm/yyyy 00:00:00
-                        {
-                            Value = ListagemAlunas[i].id_anotacao.ToString(),
-                            Text = ListagemAlunas[i].data_anotacao.ToString(),
-                        });
-                        if (data.Rows[0]["data_anotacao"].ToString() == ListagemAlunas[i].data_anotacao)
-                            ListagemAluna[i].Selected = true;
-                    }
+                        Value = ListagemAlunas[i].id_anotacao.ToString(),
+                        Text = ListagemAlunas[i].data_anotacao.ToString(),
+                    });
+                    if (data.Rows[0]["data_anotacao"].ToString() == ListagemAlunas[i].data_anotacao)
+                        ListagemAluna[i].Selected = true;
                 }
-            
+            }
+
             ViewBag.Lista = ListagemAluna;
 
         }
