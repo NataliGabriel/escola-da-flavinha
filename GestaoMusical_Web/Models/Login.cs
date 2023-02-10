617400494a9e2f21b8d8378f7218e7c857f99132
NataliGabriel <bielnatali7@gmail.com>
2023-02-10 13:28:04 -0300

    Adicionando Filtro de Datas para as Alunas

--- GestaoMusical_Web/Models/Login.cs
+++ GestaoMusical_Web/Models/Login.cs
@@ -7,8 +7,10 @@ namespace GestaoMusical_Web.Models
     {
         public Int64 IdAlunas { get; set; }
         public string usuario { get; set; }
-        
+
         public string anotacao { get; set; }
+        public string data_anotacao { get; set; }
+        public int id_anotacao { get; set; }
 
 
         public List<Login> ListaAlunas()
@@ -25,14 +27,37 @@ namespace GestaoMusical_Web.Models
                     leituras.Add(new Login
                     {
                         IdAlunas = Convert.ToInt64(Registros.Rows[Registro]["id_alunas"]),
-                        usuario = Convert.ToString(Registros.Rows[Registro]["nome_completo"]),
+                        usuario = Convert.ToString(Registros.Rows[Registro]["nome_completo"])
                     }
                     );
                 }
             }
             return leituras;
         }
+        public List<Login> ListaDatas(int alunaID)
+        {
+            List<Login> leituras = new List<Login>();
+            var sql = "select DISTINCT on (data_anotacao) data_anotacao, id_anotacao from sc_alunas.tb_anotacao WHERE id_alunas = " + alunaID + " ORDER BY data_anotacao";
+            Banco banco = new Banco();
+            DataTable Registros = banco.SelecionaDados(sql);
 
+            if (Registros.Rows.Count != 0)
+            {
+                for (int Registro = 0; Registro < Registros.Rows.Count; Registro++)
+                {
+                    if (Registros.Rows[Registro]["data_anotacao"].ToString() != "")
+                    {
+                        leituras.Add(new Login
+                        {
+                            id_anotacao = Convert.ToInt32(Registros.Rows[Registro]["id_anotacao"]),
+                            data_anotacao = Registros.Rows[Registro]["data_anotacao"].ToString().Replace(" 00:00:00", "")
+                        }
+                        );
+                    }
+                }
+            }
+            return leituras;
+        }
         public Login PesquisarAlunaId(int IDaluna)
         {
             Login leituras = new Login();
@@ -52,7 +77,7 @@ namespace GestaoMusical_Web.Models
             {
                 for (int Registro = 0; Registro < Registros.Rows.Count; Registro++)
                 {
-                //  leituras.IdAlunas = Convert.ToInt64(Registros.Rows[Registro]["id_alunas"]);
+                    //  leituras.IdAlunas = Convert.ToInt64(Registros.Rows[Registro]["id_alunas"]);
                     leituras.usuario = Convert.ToString(Registros.Rows[Registro]["id_alunas"]);
                     leituras.anotacao = Convert.ToString(Registros.Rows[Registro]["anotacao"]);
 
